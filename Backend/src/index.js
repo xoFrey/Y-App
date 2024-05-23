26d7b833ba4f95dc5857121320bff33496a1b9cf
xofrey <xizelacar@gmail.com>
2024-05-23 14:46:36 +0200

    fixed login

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,21 +13,13 @@ dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 
-app.use(morgan("dev"));
-app.use(express.json());
-
-// *routes
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/tweets", tweetRoute);
-app.use("/api/v1/comments", commentRoute);
-
 // // *cookies
 const twoMonthsinMS = 5256000000;
 const isFrontendLocalhost =
   process.env.FRONTEND_URL.startsWith("http://localhost");
 const cookieSessionSecret = process.env.COOKIE_SESSION_SECRET;
 
-app.use(cors({ origin: [process.env.FRONTEND_URL], credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.set("trust proxy", 1);
 const cookieSessionOptions = {
   name: "session",
@@ -38,6 +30,14 @@ const cookieSessionOptions = {
   secure: isFrontendLocalhost ? false : true,
 };
 app.use(cookieSession(cookieSessionOptions));
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+// *routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/tweets", tweetRoute);
+app.use("/api/v1/comments", commentRoute);
 
 try {
   await connectToDB();
